4ce4dfd29b58a8294ae136075b034442462e9810
Taufik Sulaeman Puspanegara <taufiksu@gmail.com>
2017-01-08 10:03:33 +0700

    Update 1. Upload Image Article Final

--- js/helper.js
+++ js/helper.js
@@ -68,3 +68,14 @@ function getUserInfo() {
     });
     return datas.responseJSON[0];
 }
+
+function getParamValue(name) {
+    var regexS = "[\\?&]" + name + "=([^&#]*)",
+        regex = new RegExp(regexS),
+        results = regex.exec(window.location.search);
+    if (results == null) {
+        return "";
+    } else {
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+}
